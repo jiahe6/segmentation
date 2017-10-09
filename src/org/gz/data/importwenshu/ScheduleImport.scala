53fe6b73f6753681ed45640a7cf0a2392958d4a6
YueYingWuHua <avfun126@gmail.com>
2017-10-09 16:04:58 +0800

    还差写到mongodb的程序

--- src/org/gz/data/importwenshu/ScheduleImport.scala
+++ src/org/gz/data/importwenshu/ScheduleImport.scala
@@ -10,6 +10,9 @@ import java.net.URL
 import sys.process._
 import org.apache.logging.log4j.LogManager
 import org.gz.util.Conf
+import org.gz.util.IOUtils
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
 
 /**
  * 定期导入程序
@@ -17,31 +20,41 @@ import org.gz.util.Conf
 object ScheduleImport extends Conf{
 	val log = LogManager.getLogger(this.getClass.getName())
 	val sdf = new SimpleDateFormat("yyyyMMdd")
+	lazy val mongoURI = new MongoClientURI("mongodb://gaoze:qazwsxedc!@192.168.12.161:27017/?authSource=admin")
+	lazy val mongo = new MongoClient(mongoURI)
+	private lazy val db = mongo.getDatabase("updatesdata")
+	private lazy val dbColl = db.getCollection("newdata")
 	
 	val c = Calendar.getInstance
 	c.setTime(sdf.parse(sdf.format(new Date(System.currentTimeMillis()))))
-	val wenshuPath = if(System.getProperty("os.name").toLowerCase().startsWith("win")) config.getString("importwenshu.winrarpath") else config.getString("importwenshu.linuxrarpath")
-
+	
 	def getStartTime = config.getString("starttime")
+	val c2 = Calendar.getInstance
+	c2.setTime(sdf.parse(getStartTime))
+	val (wenshuRarPath, wenshuPath) = 
+		if(System.getProperty("os.name").toLowerCase().startsWith("win")) (config.getString("importwenshu.winrarpath"), config.getString("importwenshu.winwenshupath")) 
+		else (config.getString("importwenshu.linuxrarpath"), config.getString("importwenshu.linuxwenshupath"))
 	
 	def DownloadRar(day: String) = {
-		{new URL("http://wenshu.court.gov.cn/DownLoad/FileDownLoad.aspx?action=1&userName=dsjyjy&pwd=yjy201611&dates=" + day) #> new File("D:/library/wenshu/" + day) !!}
+		{new URL("http://wenshu.court.gov.cn/DownLoad/FileDownLoad.aspx?action=1&userName=dsjyjy&pwd=yjy201611&dates=" + day) #> new File(wenshuRarPath + day + ".rar") !!}
 	}
 	
-	def unzipRar() = {
-		
+	def unzipRar(day: String) = {
+		val f = new File(wenshuRarPath + day + ".rar")
+		if (f.exists) {
+			log.info("start unzip file: " + wenshuRarPath + day + ".rar")
+			IOUtils.decompressZip(f, wenshuPath + day + "/")
+		} else {
+			log.warn("cant find the rar file:\t" + wenshuRarPath + day)
+		}
 	}
 	
-	def insertOld() = {
+	def insertOld() = {		
     val runa = new Runnable(){
       override def run(): Unit = {
       	try {
-      		val c2 = Calendar.getInstance
-					c.setTime(sdf.parse(getStartTime))
-      		if (c.after(c2))
-      			DownloadRar(sdf.format(c.getTime))        		        	
-        	c2.add(Calendar.DAY_OF_MONTH, 1)
-      		log.info(s"Downloaded wenshu${sdf.format(c.getTime)} at " + new Date(System.currentTimeMillis))      		
+					if (c.after(c2))
+      			doInsertByTime(c2)
       	} catch {
       		case e: Throwable => log.error(s"return error when downloading wenshu${sdf.format(c.getTime)} at " + new Date(System.currentTimeMillis))
       	}
@@ -49,18 +62,32 @@ object ScheduleImport extends Conf{
     }
 		val scheduler = Executors.newScheduledThreadPool(1)
     val sf = scheduler.scheduleAtFixedRate(runa, 0, 180, TimeUnit.SECONDS)
+    scheduler
   }
 
-
+  def doInsertByTime(cal: Calendar) = {
+  	//下载zip包
+  	DownloadRar(sdf.format(cal.getTime))        		        	
+ 		log.info(s"Downloaded wenshu${sdf.format(cal.getTime)} at " + new Date(System.currentTimeMillis))
+ 		//解压zip包
+  	unzipRar(sdf.format(cal.getTime))
+  	//+1s
+  	cal.add(Calendar.DAY_OF_MONTH, 1)
+  	//写到芒果里
+  }
 	
   def main(args: Array[String]): Unit = {
+  	val so = insertOld()
+  	
+  	val cn = Calendar.getInstance
+		cn.setTime(sdf.parse(sdf.format(new Date(System.currentTimeMillis()))))
     val scheduler = Executors.newScheduledThreadPool(1)
   		val r = new Runnable(){
   			override def run(): Unit = {
-  				
+  				if (!c.after(c2)) so.shutdown
+  				doInsertByTime(cn)
   			}
     }
-  	//scheduler.schedule(r, 1, TimeUnit.DAYS)
-    println(c.getTime)
+  	scheduler.scheduleAtFixedRate(r, 0, 1, TimeUnit.DAYS)
   }
 }
